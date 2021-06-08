5b46dbf0d2b65aa5e8c22789a976aca50f8a19ae
Ayush Raj <ping2r3j@gmail.com>
2021-06-08 23:01:54 +0530

    Removed some file

--- train/geektrust.js
+++ train/geektrust.js
@@ -1,5 +1,36 @@
-import { trainAStationDistance, trainBStationDistance, trainAHydDist, trainBHydDist } from './station_distance.js';
-import fs from 'fs';
+const fs =  require('fs');
+
+const trainAStationDistance = {
+    "CHN":0,
+    "SLM":350,
+    "BLR":550,
+    "KRN":900,
+    "HYB":1200,
+    "NGP":1600,
+    "ITJ":1900,
+    "BPL":2000,
+    "AGA":2500,
+    "NDL":2700
+}
+
+const trainBStationDistance = {
+    "TVC":0,
+    "SRR":300,
+    "MAQ":600,
+    "MAO":1000,
+    "PNE":1400,
+    "HYB":2000,
+    "NGP":2400,
+    "ITJ":2700,
+    "BPL":2800,
+    "PTA":3800,
+    "NJP":4200,
+    "GHY":4700
+}
+
+const trainAHydDist = 1200;
+
+const trainBHydDist = 2000;
 
 var filename = process.argv[2];
 
@@ -18,8 +49,6 @@ function readFromFile() {
 }
 
 const [trainA, trainB] = readFromFile();
-
-let stationAfterHyd = {}
 
 const stationDistFrmHyd = () => {
     let ans = {};
@@ -42,17 +71,19 @@ const stationDistFrmHyd = () => {
     return sortedDistance;
 }
 
+let stationAfterHyd = {}
+
 const findOrderOfStation = (train, id, stationDistance, dist) => {
 
     let orderOfBogies = []
 
-    train.map((station) => {
+    for(let station of train){
         station = cleanString(station)
         if (stationDistance[station] == undefined || stationDistance[station] >= dist) {
             orderOfBogies.push([station, stationDistance[station]]);
         }
-    })
-
+    }
+    
     let ans = "";
 
     if (id === 'A') {
